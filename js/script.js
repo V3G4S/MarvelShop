50f56c615af268ad683030af429ba2fff29d2d1f
ANTONY VALENTIM GARCIA DOS SANTOS <antony_v_santos@estudante.sesisenai.org.br>
2024-06-19 17:16:39 -0300

    update1

--- js/script.js
+++ js/script.js
@@ -1,133 +1,38 @@
-// let login = '', senha, article, div, h3, p1, input, p2, span, aLink, main, section, footer, h2, p3, span2
-// let usr = []
-// let snh = []
-// let produto = []
-// if(localStorage.prodArr){
-//     produto = JSON.parse(localStorage.getItem('prodArr'))
-// }
-// let cod = []
-// if(localStorage.codArr){
-//     cod = JSON.parse(localStorage.getItem('codArr'))
-// }
-// let preco = []
-// if(localStorage.precoArr){
-//     preco = JSON.parse(localStorage.getItem('precoArr'))
-// }
-// let link = []
-// if(localStorage.linkArr){
-//     link = JSON.parse(localStorage.getItem('linkArr'))
-// }
-// let qtd = []
-// if(localStorage.qtdArr){
-//     qtd = JSON.parse(localStorage.getItem('qtdArr'))
-// }
-// let total = []
-// if(localStorage.totCompArr){
-//     link = JSON.parse(localStorage.getItem('totCompArr'))
-// }
-// main = document.createElement('main')
-// main.setAttribute('class', 'container')
-// document.body.append(main)
-// section = document.createElement('section')
-// section.setAttribute('class', 'products-container')
-// main.append(section)
-// for(i in produto){
-//     article = document.createElement('article')
-//     article.setAttribute('class', 'card')
-//     section.append(article)
-//     div = document.createElement('div')
-//     div.setAttribute('class', 'product-image')
-//     div.setAttribute('id', 'img-' + i)
-//     article.append(div)
-//     document.getElementById('img-' + i).style.backgroundImage = 'url (img/img' + i + '.jpg'
-//     h3 = document.createElement('h3')
-//     h3.setAttribute('id', 'nome' + i)
-//     article.append(h3)
-//     p1 = document.createElement('p')
-//     p1.innerHTML = 'Qtd: '
-//     article.append(p1)
-//     input = document.createElement('input')
-//     input.setAttribute('type', 'number')
-//     input.setAttribute('value', '1')
-//     input.setAttribute('min', '0')
-//     input.setAttribute('max', '10')
-//     input.setAttribute('id', 'qtd-' + i)
-//     p1.append(input)
-//     article.append(p1)
-//     p2 = document.createElement('p')
-//     p2.innerHTML = 'R$ '
-//     span.setAttribute(cod)
-// }
-// function criaLogin(){
-//     if(localStorage.usrArr){
-//         usr = JSON.parse(localStorage.getItem('usrArr'))
-//     }
-//     if(localStorage.snhArr){
-//         snh = JSON.parse(localStorage.getItem('snhArr'))
-//     }
-//     let novoUsr = prompt("Nome de usuário: ")
-//     usr.push(novoUsr)
-//     localStorage.usrArr = JSON.stringify(usr)
-//     let novaSnh = prompt("Senha: ")
-//     snh.push(novaSnh)
-//     localStorage.snhArr = JSON.stringify(snh)
-//     if(usr.includes(novoUsr) && snh.includes(novaSnh)){
-//         alert("Conta criada com sucesso!")
-//     }else{
-//         alert("Conta não pode ser criada")
-//     }
-// }
-// function abreTelaLogin(){
-//     if(localStorage.usrArr){
-//         usr = JSON.parse(localStorage.getItem('usrArr'))
-//     }
-//     if(localStorage.snhArr){
-//         snh = JSON.parse(localStorage.getItem('snhArr'))
-//     }
-//     login = prompt("Nome de usuário: ")
-//     senha = prompt("Senha: ")
-//     let indUsr = usr.indexOf(login)
-//     if(usr[indUsr] == login && snh[indUsr] == senha){
-//         document.querySelector("#login").innerHTML = `Bem-vindo(a), ${login}`
-//     }else{
-//         alert("Digite o usuário/senha correto")
-//     }
-// }
-
-let login = '', senha, article, div, h3, p1, input, span, aLink, main, section, footer, h2, p3, span2
+let login = '', senha, article, div, h3, p1, input, p2, span, aLink, main, section, footer, h2, p3, span2, aLink2
 let usr = []
 let snh = []
 let produto = []
-if(localStorage.prodArr) {
+if(localStorage.prodArr){
     produto = JSON.parse(localStorage.getItem('prodArr'))
 }
 let cod = []
-if(localStorage.codArr) {
+if(localStorage.codArr){
     cod = JSON.parse(localStorage.getItem('codArr'))
 }
 let preco = []
-if(localStorage.precoArr) {
+if(localStorage.precoArr){
     preco = JSON.parse(localStorage.getItem('precoArr'))
 }
 let link = []
-if(localStorage.linkArr) {
+if(localStorage.linkArr){
     link = JSON.parse(localStorage.getItem('linkArr'))
 }
 let qtd = []
-if(localStorage.qtdArr) {
+if(localStorage.qtdArr){
     qtd = JSON.parse(localStorage.getItem('qtdArr'))
 }
 let totalCompra = []
-if(localStorage.totCompArr) {
+if(localStorage.totCompArr){
     totalCompra = JSON.parse(localStorage.getItem('totCompArr'))
 }
+
 main = document.createElement('main')
 main.setAttribute('class', 'container')
 document.body.append(main)
 section = document.createElement('section')
 section.setAttribute('class', 'products-container')
 main.append(section)
-for (i in produto) {
+for(i in produto){
     article = document.createElement('article')
     article.setAttribute('class', 'card')
     section.append(article)
@@ -135,9 +40,10 @@ for (i in produto) {
     div.setAttribute('class', 'product-image')
     div.setAttribute('id', 'img-' + i)
     article.append(div)
-    document.getElementById('img-' + i).style.backgroundImage = 'url(/img/img' + i + '.jpg)'
+    document.getElementById('img-' + i).style.backgroundImage = 'url(imagens/img' + i + '.jpg)'
     h3 = document.createElement('h3')
     h3.setAttribute('id', 'nome' + i)
+    h3.innerHTML = produto[i]
     article.append(h3)
     p1 = document.createElement('p')
     p1.innerHTML = 'Qtd: '
@@ -146,62 +52,117 @@ for (i in produto) {
     input.setAttribute('type', 'number')
     input.setAttribute('value', '1')
     input.setAttribute('min', '1')
-    input.setAttribute('max', '100')
+    input.setAttribute('max', '10')
     input.setAttribute('id', 'qtd-' + i)
     p1.append(input)
     article.append(p1)
     p2 = document.createElement('p')
-    p2.innerHTML = 'R$: '
+    p2.innerHTML = 'R$ '
     span = document.createElement('span')
-    span.setAttribute(cod[i])
+    span.setAttribute('id', cod[i])
     span.setAttribute('class', 'bold')
     span.innerHTML = preco[i].toFixed(2).replace('.', ',')
     p2.append(span)
     article.append(p2)
     aLink = document.createElement('a')
-    aLink.setAttribute('onclick', 'compra(' + "'" + 'qtd-' + i + "'" + ',' + "'" + cod[i] + "'" + "," + i + ")")
+    aLink.setAttribute('onclick', "compra(" + "'" + 'qtd-' + i + "'" + ',' + "'" + cod[i] + "'" + ',' + i + ")")
     aLink.setAttribute('class', 'btn')
-    aLink.setAttribute('href', 'http://www.amazon.com.br/' + link[i])
+    aLink.setAttribute('href', 'http://www.amazon.com.br/&#39;' + link[i])
     aLink.setAttribute('target', '_blank')
-    aLink.innerHTML('Comprar')
+    aLink.innerHTML = 'Comprar'
     article.append(aLink)
 }
-function criaLogin() {
-    if (localStorage.usrArr) {
+footer = document.createElement('footer')
+footer.setAttribute('id', 'rodape')
+document.body.append(footer)
+h2 = document.createElement('h2')
+h2.innerHTML = 'Informações sobre o site'
+footer.append(h2)
+p3 = document.createElement('p')
+p3.innerHTML = 'Direitos reservados &copy; 2024'
+footer.append(p3)
+aLink2 = document.createElement('a')
+aLink2.setAttribute('id', 'adm')
+aLink2.setAttribute('href', 'atualizacao.html')
+aLink2.innerHTML = 'Administração'
+footer.append(aLink2)
+
+function getDados(){
+    if(localStorage.qtdArr){
+       qtd = JSON.parse(localStorage.getItem('qtdArr'))
+    }
+    qtd.push(0)
+    localStorage.qtdArr = JSON.stringify(qtd)
+    if(localStorage.totCompArr){
+        totalCompra = JSON.parse(localStorage.getItem('totCompArr'))
+     }
+     totalCompra.push(0)
+     localStorage.totCompArr = JSON.stringify(totalCompra)
+    if(localStorage.prodArr){
+        produto = JSON.parse(localStorage.getItem('prodArr'))
+     }
+     let prod = document.querySelector('#produto').value
+     produto.push(prod)
+     localStorage.prodArr = JSON.stringify(produto)
+     document.querySelector('#produto').value = ''
+     if(localStorage.codArr){
+        cod = JSON.parse(localStorage.getItem('codArr'))
+     }
+     let codig = document.querySelector('#codigo').value
+     cod.push(codig)
+     localStorage.codArr = JSON.stringify(cod)
+     document.querySelector('#codigo').value = ''
+     if(localStorage.precoArr){
+        preco = JSON.parse(localStorage.getItem('precoArr'))
+     }
+     let prec = document.querySelector('#preco').value
+     preco.push(parseFloat(prec.replace(',', '.')))
+     localStorage.precoArr = JSON.stringify(preco)
+     document.querySelector('#preco').value = ''
+     if(localStorage.linkArr){
+        link = JSON.parse(localStorage.getItem('linkArr'))
+     }
+     let lnk = document.querySelector('#linkAmazon').value
+     link.push(lnk)
+     localStorage.linkArr = JSON.stringify(link)
+     document.querySelector('#linkAmazon').value = ''
+     alert("Dados inseridos com sucesso!")
+}
+
+function criaLogin(){
+    if(localStorage.usrArr){
         usr = JSON.parse(localStorage.getItem('usrArr'))
     }
-    if (localStorage.snhArr) {
+    if(localStorage.snhArr){
         snh = JSON.parse(localStorage.getItem('snhArr'))
     }
-    let novoUsr = prompt('Login:')
+    let novoUsr = prompt("login: ")
     usr.push(novoUsr)
     localStorage.usrArr = JSON.stringify(usr)
-
-    let novaSnh = prompt('Senha:')
+    let novaSnh = prompt("senha: ")
     snh.push(novaSnh)
     localStorage.snhArr = JSON.stringify(snh)
-
-    if (usr.includes(novoUsr) && snh.includes(novaSnh)) {
-        alert('Login criado com sucesso!')
-    } else {
-        alert('Login já criado!')
+    if(usr.includes(novoUsr) && snh.includes(novaSnh)){
+        alert("Login criado com sucesso!")
+    }else{
+        alert("Login não pôde ser criado!")
     }
 }
-function abreTelaLogin() {
 
-    if (localStorage.usrArr) {
+function abreTelaLogin(){
+    if(localStorage.usrArr){
         usr = JSON.parse(localStorage.getItem('usrArr'))
     }
-    if (localStorage.snhArr) {
+    if(localStorage.snhArr){
         snh = JSON.parse(localStorage.getItem('snhArr'))
     }
-    login = prompt('Login:')
-    senha = prompt('Senha:')
-
+    login = prompt("login: ")
+    senha = prompt("login: ")
     let indUsr = usr.indexOf(login)
-    if (usr[indUsr] == login && snh[indUsr] == senha) {
-        document.querySelector('#log').innerHTML = `Bem vindo, ${login}`
-    } else {
-        alert('Usuário ou senha incorreto!')
+    if(usr[indUsr] == login && snh[indUsr] == senha){
+        document.querySelector("#login").innerHTML = `Bem-vindo, ${login}`
+    }else{
+        alert("Digite um usuário/senha correto!")
     }
+
 }
